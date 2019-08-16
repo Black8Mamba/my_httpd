35cf6219c11a555407f1d416bc735a991c8b783a
JiYongjie <jyj_student@163.com>
2019-08-16 14:11:06 +0800

    add error.h

---
+++ error.h
@@ -0,0 +1,15 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-08-16 14:10:35
+ * @LastEditTime: 2019-08-16 14:10:49
+ * @LastEditors: Please set LastEditors
+ */
+
+#ifndef ERROR_H
+#define ERROR_H
+
+#define TK_OK       0
+#define TK_ERROR    -1
+
+#endif
